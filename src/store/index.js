b1521a3795ce87da82a440468532455d143b1f53
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-09-27 11:33:57 +0300

    provided "reset" function

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,23 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 
+const moviesSlice = createSlice({
+    name: 'movie',
+    initialState: [],
+    reducers: {
+        addMovie(state, action) {
+            state.push(action.payload)
+        },
+        removeMovie(state, action) {
+            //action.payload === string, the movie we want to remove
+            const index = state.indexOf(action.payload)
+            state.splice(index, 1)
+        },
+        reset(state, action) {
+            return []
+        }
+    }
+})
+
 const songsSlice = createSlice({
     // slice:
     //   1   creates a set of 'action-creator' functions for us
@@ -16,24 +34,14 @@ const songsSlice = createSlice({
             //action.payload === string, the song we want to remove
             const index = state.indexOf(action.payload)
             state.splice(index, 1)
-        },
+        }
+    },
+    extraReducers: (builder) => {
+        builder.addCase('movie/reset', () => {
+            return []
+        })
     }
 })
-const moviesSlice = createSlice({
-    name: 'movie',
-    initialState: [],
-    reducers: {
-        addMovie(state, action) {
-            state.push(action.payload)
-        },
-        removeMovie(state, action) {
-            //action.payload === string, the movie we want to remove
-            const index = state.indexOf(action.payload)
-            state.splice(index, 1)
-        },
-    }
-})
-
 
 const store = configureStore({
     reducer: {
@@ -52,7 +60,6 @@ const finalState = store.getState()
 console.log(JSON.stringify(finalState));
 
 
-
 export { store }
 export const { addSong, removeSong } = songsSlice.actions
-export const { addMovie, removeMovie } = moviesSlice.actions
+export const { addMovie, removeMovie, reset } = moviesSlice.actions
